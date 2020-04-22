2c1492a88636449dd4fed251a13434ce5c80d9e9
Jens Lauterbach <jenslauterbach@icloud.com>
2020-04-22 20:16:37 +0200

    Improve Documentation of DecodingErrors

--- Sources/Base32Kit/Errors.swift
+++ Sources/Base32Kit/Errors.swift
@@ -1,6 +1,7 @@
 extension Base32 {
     
-    public enum DecodingError: Error, Equatable {
+    /// Errors that can be thrown during decoding of a Base 32 encoded string.
+    enum DecodingError: Error, Equatable {
         
         /// The `String` to decode has invalid length. A string that should be decoded should have a length that is a multiple of 8
         /// (e.g. 8 characters, 16, 24 ... 80, 96, etc.)
@@ -8,12 +9,19 @@ extension Base32 {
         
         /// Thrown when the encoded `String` contains invalid characters.
         ///
-        /// Base 32 Encoding only supports a very limited alphabet to which input data can be encoded to. If a given _encoded_ `String` contains characters that
-        /// are not part of this alphabet, this error is thrown.
+        /// Base 32 Encoding only supports a very limited alphabet to which input data can be encoded to. If a given _encoded_ `String` contains characters
+        /// that are not part of this alphabet, this error is thrown.
         ///
-        /// The error contains the invalid `Character`.
+        /// The error contains an array of all the invalid `Character`s found.
         case invalidCharacter([Character])
         
+        /// Thrown when the encoded  `String` contains one or more invalid padding characters (`=`).
+        ///
+        /// Padding characters are only allowed at the end of the encoded string and no other character is allowed to follow.
+        ///
+        /// Examples:
+        /// - OK: `"MZXQ===="`
+        /// - Not OK: `"M=XQ===="`
         case invalidPaddingCharacters
     }
 }
